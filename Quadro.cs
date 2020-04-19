d0fd245fd3ce384365a6e3a9e323796f69165d02
Sashca-k <62010729+Sashca-k@users.noreply.github.com>
2020-04-19 11:30:41 +0300

    Add files via upload

---
+++ Quadro.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Quadro
+    {
+        private double a;
+        private double b;
+        private double c;
+        public double A
+        {
+            get { return a; }
+            set { if (value > 0) a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { if (value > 0) b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { if (value > 0) c = value; }
+        }
+        public Quadro()
+        {
+            A = 0;
+            B = 0;
+            C = 0;
+        }
+        public Quadro (double x,double y, double z)
+        {
+            A = x;
+            B = y;
+            C = z;
+        }
+        public double dis()
+        {
+            double D = Math.Pow(b,2)-4*a* c;
+            if (D>0 || D == 0)
+            {
+               double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+               double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+            }
+            else 
+            {
+                Console.WriteLine("Корней нет");
+            }   
+            return D;
+        }
+    }
+}
---
+++ QuadroTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace TestQuadro
+{
+    [TestClass]
+    public class QuadroTest
+    {
+        [TestMethod]
+        public void dis()
+        {
+            double a = 5;
+            double b = 3;
+            double c = 7;
+            double expected = -131;
+            Quadro q = new Quadro(a, b, c);
+            double actual = q.dis();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
